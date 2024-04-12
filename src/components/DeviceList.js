153582daf749462f13b4fb16cb0b0aa1650459ea
unknown <kamolov1575@gmail.com>
2024-04-12 20:41:07 +0500

    device list image remove base app api url

--- src/components/DeviceList.js
+++ src/components/DeviceList.js
@@ -17,7 +17,7 @@ const Shop = observer(() => {
                             <div key={product.id} className="group relative">
                                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                     <img
-                                        src={process.env.REACT_APP_API_URL + product.img}
+                                        src={product.img}
                                         className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                                     />
                                 </div>
