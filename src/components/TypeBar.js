9799875d9360436d9dcf788a1a09f1b6715fc027
unknown <kamolov1575@gmail.com>
2024-04-12 22:37:35 +0500

    version 1.0

--- src/components/TypeBar.js
+++ src/components/TypeBar.js
@@ -10,7 +10,7 @@ const TypeBar = observer(() => {
     const {device} = useContext(Context)
 
     return (
-    <div>
+      <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8 ">
         <nav className="flex space-x-4" aria-label="Tabs">
           {device.brands.map((brand) => (
             <button
@@ -18,7 +18,7 @@ const TypeBar = observer(() => {
               onClick={() => device.setSelectedBrand(brand)}
               className={classNames(
                 brand.id === device.selectedBrand.id ? 'bg-gray-100 text-gray-700' : 'text-gray-500 hover:text-gray-700',
-                'rounded-md px-3 py-2 text-sm font-medium'
+                'rounded-md px-3 py-2 text-sm font-medium bg-gray-50'
               )}
               aria-current={brand.current ? 'page' : undefined}
             >
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -14,10 +14,10 @@ const Shop = observer(() => {
   }, [])
 
     return (
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-            <TypeBar/>
-            <DeviceList />
-        </div>
+      <>
+        <TypeBar/>
+        <DeviceList />
+      </>
     )
   })
 
