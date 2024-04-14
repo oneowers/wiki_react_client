eeb87d7e0d4abe56b9b9443f1924b07979336837
unknown <kamolov1575@gmail.com>
2024-04-14 07:24:38 +0500

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -72,7 +72,7 @@ const NavBar = observer(() => {
                   className="text-sm font-medium relative rounded-md bg-gray-950 p-2 text-gray-400 hover:text-white hover:bg-gray-900 focus:outline-none"
                 >
                     Админ
-                </Link>}
+                </Link>} 
                 
                 <button
                   className="relative rounded-full bg-gray-950 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
