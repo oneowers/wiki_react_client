7ffd7eff0ae8bbecc3c6409e35b9d1fdbb6f246c
unknown <kamolov1575@gmail.com>
2024-04-14 06:31:04 +0500

    first commit

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -204,6 +204,7 @@ const Auth = observer(() => {
                   autoComplete="off"
                   required
                   value={code}
+                  autocomplete="one-time-code"
                   onChange={handleCodeChange}
                   maxLength={5} // Устанавливаем максимальную длину вводимого кода
                   className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
