c10a4fc26a71599ee39e5e4a5b25ed1a05226724
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-04-22 12:48:53 +0500

    commit message

--- src/components/DeviceList.js
+++ src/components/DeviceList.js
@@ -10,14 +10,16 @@ const Shop = observer(() => {
   const navigate = useNavigate();
   const [popularDevices, setPopularDevices] = useState({});
 
-
-  useEffect(() =>{
-    fetchDevices(undefined, undefined, undefined, 12, "views").then(data => setPopularDevices(data));
+  useEffect(() => {
+    fetchDevices(undefined, undefined, undefined, 12, "views").then((data) =>
+      setPopularDevices(data)
+    );
   }, []);
 
   return (
     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-2 lg:grid-cols-1 xl:gap-x-8">
-      {popularDevices.count && popularDevices.rows.map((product) => (
+      {popularDevices.count &&
+        popularDevices.rows.map((product) => (
           <div
             onClick={() => navigate(DEVICE_ROUTE + "/" + product.id)}
             key={product.id}
@@ -30,7 +32,15 @@ const Shop = observer(() => {
               />
             </div>
             <div className="flex flex-col justify-between">
-              <span className="text-sm font-bold uppercase text-center text-pink-500 bg-pink-50 px-4 py-0.5 rounded-full">
+              <span
+                aria-hidden="true"
+                className={classNames(
+                  `text-sm font-bold uppercase text-center ${
+                    device.brands[product.brandId] &&
+                    device.brands[product.brandId].color
+                  } px-4 py-0.5 rounded-full`
+                )}
+              >
                 <a href={product.href}>
                   <span aria-hidden="true" className="absolute inset-0" />
                   {device.brands[product.brandId] &&
