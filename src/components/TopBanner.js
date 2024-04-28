c58ef82a9c1813aa53e692304c8940d52f70695b
unknown <kamolov1575@gmail.com>
2024-04-28 21:03:32 +0500

    add new modeal

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,9 +1,14 @@
 import { XMarkIcon } from '@heroicons/react/20/solid'
 import { ABOUT_ROUTE } from '../utils/consts'
 import { Link } from 'react-router-dom'
+import Participate from '../modals/Participate'
+import { useState } from 'react';
 
 export default function TopBanner() {
+  const [participateVisible, setParticipateVisible] = useState(false);
+
   return (
+    <>
     <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <div
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
@@ -38,8 +43,8 @@ export default function TopBanner() {
           Присоединяйтесь к нам в Экспо с 28 по 30 мая, нажмите чтобы узнать больше.
         </p>
         <Link
-          to={ABOUT_ROUTE}
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          onClick={() => setParticipateVisible(true)}
         >
           Участвовать <span aria-hidden="true">&rarr;</span>
         </Link>
@@ -51,5 +56,7 @@ export default function TopBanner() {
         </button>
       </div>
     </div>
+    <Participate show={participateVisible} onHide={ () => setParticipateVisible(false)} />
+    </>
   )
 }
