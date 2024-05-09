a2ccec030f9d7de39edd10c97117218fa2a236bf
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-05-09 21:30:31 +0500

    models fix

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -56,7 +56,7 @@ export default function TopBanner() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-lg font-medium leading-6 text-white">
-          <strong className="font-semibold">Leather 2024</strong>
+          <strong className="font-semibold">UzCharmExpo-LeatherGoods2024</strong>
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
