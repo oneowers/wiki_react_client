8d672aadb5b55b509fad6d3dea9050f4aa893a65
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-05-21 08:51:42 +0500

    add sitemap

--- src/http/participantApi.js
+++ src/http/participantApi.js
@@ -1,6 +1,20 @@
-import { $authHost } from "./index.js";
 
-export const createParticipant = async (participant) => {
-  const { data } = await $authHost.post("/api/participant", participant);
-  return data;
-};
+    import { toast } from 'react-toastify';
+    import axios from 'axios';
+import { $authHost, $host } from "./index.js";
+
+    
+    export const createParticipant = async (participant) => {
+      try {
+        const { data } = await $authHost.post("/api/participant", participant);
+        return data;
+      } catch (error) {
+        if (error.response && error.response.data && error.response.data.error) {
+          toast.error(error.response.data.error);
+        } else {
+          toast.error("An unexpected error occurred. Please try again later.");
+        }
+        console.error("Error creating participant:", error);
+      }
+    };
+    
