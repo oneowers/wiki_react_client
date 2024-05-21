79fced785ae7cb59be83586c862f80c7f776dc4c
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-05-21 06:45:02 +0500

    add sitemap

--- src/elements/DropdownSelect.js
+++ src/elements/DropdownSelect.js
@@ -9,7 +9,7 @@ const DropdownSelect = ({ label, onChange, selected, arrayList }) => {
       <Menu as="div" className="mt-1 relative inline-block text-left w-full">
         <div>
           <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm border border-gray-300 hover:bg-gray-50">
-            {selected || "Выбрать страну"}
+            {selected.common || "Выбрать страну"}
             <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
           </Menu.Button>
         </div>
@@ -25,18 +25,18 @@ const DropdownSelect = ({ label, onChange, selected, arrayList }) => {
           <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none max-h-60 overflow-y-auto">
             <div className="py-1">
               {arrayList.map((countryItem) => (
-                <Menu.Item key={countryItem.name.common}>
+                <Menu.Item key={countryItem.id}>
                   <button
-                    onClick={() => onChange(countryItem.name.common)}
+                    onClick={() => onChange(countryItem)}
                     className={
-                      countryItem.name.common === selected
+                      countryItem.id === selected.id
                         ? "bg-gray-50 text-gray-900 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900 w-full truncate overflow-ellipsis"
                         : "text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900 w-full truncate overflow-ellipsis"
                     }
                   >
                     <div className="flex">
-                      <img className="w-8 h-5 mr-3" src={countryItem.flags.png} alt={countryItem.name.common}/>
-                      {countryItem.name.common}
+                      {/* <img className="w-8 h-5 mr-3" src={countryItem.flags.png} alt={countryItem.name.common}/> */}
+                      {countryItem.common}
                     </div>
                   </button>
                 </Menu.Item>
--- src/pages/Participant.js
+++ src/pages/Participant.js
@@ -1,27 +1,199 @@
 import React, { useState, useEffect } from "react";
 import { createParticipant } from "../http/participantApi.js";
-import { Modal, InputField, DropdownSelect } from "../elements/index.js";
-import { fetchCountries } from "../http/countryesApi.js";
+import { InputField, DropdownSelect } from "../elements/index.js";
 import { Link } from "react-router-dom";
 import { ABOUT_ROUTE, DEVICE_ROUTE, SHOP_ROUTE } from "../utils/consts.js";
+import QRCode from "qrcode.react";
 
 const Participate = ({ show, onHide }) => {
-  const [countryes, setCountryes] = useState([]);
-  const [activeTab, setActiveTab] = useState("GUEST");
-  const [country, setCountry] = useState("");
-  const [region, setRegion] = useState(""); // New state for manual input of region
-  const [companyName, setCompanyName] = useState("");
-  const [address, setAddress] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [employeeName, setEmployeeName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(false);
+  // Состояние для хранения текущего выбранного языка
+  const [country, setCountry] = useState({ common: "🇺🇿 O'zbekiston", id: "uz" }); // Define country state
+  const [region, setRegion] = useState(""); // Define region state
+  const [employeeName, setEmployeeName] = useState(""); // Define employeeName state
+  const [phoneNumber, setPhoneNumber] = useState(""); // Define phoneNumber state
+  const [email, setEmail] = useState(""); // Define email state
+  const [success, setSuccess] = useState(false); // Define success state
+  const [id, setId] = useState(0); // Define id state
+  
+  // Словарь переводов для различных языков
+  const translations= {
+    uz: {
+      countryOptions: [
+        { common: "🇺🇿 O'zbekiston", id: "uz" },
+        { common: "🇷🇺 Rossiya", id: "ru" },
+        { common: "🇺🇸 AQSH", id: "us" },
+        { common: "🇮🇹 Italiya", id: "it" },
+        { common: "🇨🇳 Xitoy", id: "cn" },
+        { common: "🇮🇷 Eron", id: "ir" },
+        { common: "🇧🇾 Belarus", id: "by" },
+        { common: "🇩🇪 Germaniya", id: "de" }
+      ],
+      participateTitle: "Chiqishga qatnashish",
+      aboutExhibition: "Nazorat haqida ma'lumot",
+      fullNameLabel: "To'liq ism",
+      phoneNumberLabel: "Telefon raqami",
+      emailLabel: "Elektron pochta",
+      submitButton: "Jo'natish",
+      successMessage: "Muvaffaqiyatli ro'yxatdan o'tdingiz",
+      returnHomeLink: "Bosh sahifaga qaytish",
+      pleaseDontClose: "Iltimos, ushbu sahifani yopmang"
+    },
+    ru: {
+      countryOptions: [
+        { common: "🇺🇿 Узбекистан", id: "uz" },
+        { common: "🇷🇺 Россия", id: "ru" },
+        { common: "🇺🇸 США", id: "us" },
+        { common: "🇮🇹 Италия", id: "it" },
+        { common: "🇨🇳 Китай", id: "cn" },
+        { common: "🇮🇷 Иран", id: "ir" },
+        { common: "🇧🇾 Беларусь", id: "by" },
+        { common: "🇩🇪 Германия", id: "de" }
+      ],
+      participateTitle: "Участвовать на выставке",
+      aboutExhibition: "Информация о выставке",
+      fullNameLabel: "Полное имя",
+      phoneNumberLabel: "Телефонный номер",
+      emailLabel: "Электронная почта",
+      submitButton: "Отправить",
+      successMessage: "Вы успешно зарегистрировались",
+      returnHomeLink: "Вернуться на главную страницу",
+      pleaseDontClose: "Пожалуйста, не закрывайте эту страницу"
+    },
+    us: {
+      countryOptions: [
+        { common: "🇺🇿 Uzbekistan", id: "uz" },
+        { common: "🇷🇺 Russia", id: "ru" },
+        { common: "🇺🇸 USA", id: "us" },
+        { common: "🇮🇹 Italy", id: "it" },
+        { common: "🇨🇳 China", id: "cn" },
+        { common: "🇮🇷 Iran", id: "ir" },
+        { common: "🇧🇾 Belarus", id: "by" },
+        { common: "🇩🇪 Germany", id: "de" }
+      ],
+      participateTitle: "Participate in the exhibition",
+      aboutExhibition: "About the exhibition",
+      fullNameLabel: "Full name",
+      phoneNumberLabel: "Phone number",
+      emailLabel: "Email",
+      submitButton: "Submit",
+      successMessage: "You have successfully registered",
+      returnHomeLink: "Return to the homepage",
+      pleaseDontClose: "Please do not close this page"
+    },
+    it: {
+      countryOptions: [
+        { common: "🇺🇿 Uzbekistan", id: "uz" },
+        { common: "🇷🇺 Russia", id: "ru" },
+        { common: "🇺🇸 USA", id: "us" },
+        { common: "🇮🇹 Italy", id: "it" },
+        { common: "🇨🇳 China", id: "cn" },
+        { common: "🇮🇷 Iran", id: "ir" },
+        { common: "🇧🇾 Belarus", id: "by" },
+        { common: "🇩🇪 Germany", id: "de" }
+      ],
+      participateTitle: "Partecipare alla mostra",
+      aboutExhibition: "Informazioni sulla mostra",
+      fullNameLabel: "Nome completo",
+      phoneNumberLabel: "Numero di telefono",
+      emailLabel: "Indirizzo email",
+      submitButton: "Invia",
+      successMessage: "Ti sei registrato con successo",
+      returnHomeLink: "Torna alla homepage",
+      pleaseDontClose: "Per favore, non chiudere questa pagina"
+    },
+    cn: {
+      countryOptions: [
+        { common: "🇺🇿 乌兹别克斯坦", id: "uz" },
+        { common: "🇷🇺 俄罗斯", id: "ru" },
+        { common: "🇺🇸 美国", id: "us" },
+        { common: "🇮🇹 意大利", id: "it" },
+        { common: "🇨🇳 中国", id: "cn" },
+        { common: "🇮🇷 伊朗", id: "ir" },
+        { common: "🇧🇾 白俄罗斯", id: "by" },
+        { common: "🇩🇪 德国", id: "de" }
+      ],
+      participateTitle: "参加展览",
+      aboutExhibition: "关于展览",
+      fullNameLabel: "全名",
+      phoneNumberLabel: "电话号码",
+      emailLabel: "电子邮件",
+      submitButton: "提交",
+      successMessage: "您已成功注册",
+      returnHomeLink: "返回首页",
+      pleaseDontClose: "请不要关闭此页面"
+    },
+    ir: {
+      countryOptions: [
+        { common: "🇺🇿 ازبکستان", id: "uz" },
+        { common: "🇷🇺 روسیه", id: "ru" },
+        { common: "🇺🇸 آمریکا", id: "us" },
+        { common: "🇮🇹 ایتالیا", id: "it" },
+        { common: "🇨🇳 چین", id: "cn" },
+        { common: "🇮🇷 ایران", id: "ir" },
+        { common: "🇧🇾 بلاروس", id: "by" },
+        { common: "🇩🇪 آلمان", id: "de" }
+      ],
+      participateTitle: "شرکت در نمایشگاه",
+      aboutExhibition: "درباره نمایشگاه",
+      fullNameLabel: "نام کامل",
+      phoneNumberLabel: "شماره تلفن",
+      emailLabel: "ایمیل",
+      submitButton: "ارسال",
+      successMessage: "شما با موفقیت ثبت نام کرده اید",
+      returnHomeLink: "بازگشت به صفحه اصلی",
+      pleaseDontClose: "لطفاً این صفحه را بسته نکنید"
+    },
+    by: {
+      countryOptions: [
+        { common: "🇺🇿 Узбекистан", id: "uz" },
+        { common: "🇷🇺 Россия", id: "ru" },
+        { common: "🇺🇸 США", id: "us" },
+        { common: "🇮🇹 Италия", id: "it" },
+        { common: "🇨🇳 Китай", id: "cn" },
+        { common: "🇮🇷 Иран", id: "ir" },
+        { common: "🇧🇾 Беларусь", id: "by" },
+        { common: "🇩🇪 Германия", id: "de" }
+      ],
+      participateTitle: "Удзельнічаць у выставе",
+      aboutExhibition: "Інфармацыя пра выставу",
+      fullNameLabel: "Поўнае імя",
+      phoneNumberLabel: "Тэлефонны нумар",
+      emailLabel: "Электронная пошта",
+      submitButton: "Адправіць",
+      successMessage: "Вы паспяхова зарэгістраваліся",
+      returnHomeLink: "Вярнуцца на галоўную старонку",
+      pleaseDontClose: "Калі ласка, не зачыняйце гэтую старонку"
+    },
+    de: {
+      countryOptions: [
+        { common: "🇺🇿 Usbekistan", id: "uz" },
+        { common: "🇷🇺 Russland", id: "ru" },
+        { common: "🇺🇸 USA", id: "us" },
+        { common: "🇮🇹 Italien", id: "it" },
+        { common: "🇨🇳 China", id: "cn" },
+        { common: "🇮🇷 Iran", id: "ir" },
+        { common: "🇧🇾 Weißrussland", id: "by" },
+        { common: "🇩🇪 Deutschland", id: "de" }
+      ],
+      participateTitle: "An der Ausstellung teilnehmen",
+      aboutExhibition: "Über die Ausstellung",
+      fullNameLabel: "Vollständiger Name",
+      phoneNumberLabel: "Telefonnummer",
+      emailLabel: "E-Mail",
+      submitButton: "Einreichen",
+      successMessage: "Sie haben sich erfolgreich registriert",
+      returnHomeLink: "Zurück zur Startseite",
+      pleaseDontClose: "Bitte schließen Sie diese Seite nicht"
+    }
+  }
+  
+  
+  
 
+  // Функция для обработки добавления участника
   const addParticipant = async () => {
     const participant = {
-      state: country || region, // Use either the selected country or the manually input region
+      state: country || region, // Используйте выбранную страну или регион, если введен вручную
       full_name: employeeName,
       phone_number: phoneNumber,
     };
@@ -29,186 +201,89 @@ const Participate = ({ show, onHide }) => {
     try {
       const data = await createParticipant(participant);
       console.log("Participant created:", data);
-      setError(null);
-      setSuccess(true)
+      setId(data.id);
+      setSuccess(true);
     } catch (error) {
       console.error("Error creating participant:", error);
-      setError(error.response?.data?.error || "Could not create participant");
     }
-  };
-
-  useEffect(() => {
-    fetchCountries()
-      .then((data) => setCountryes(data))
-      .catch((error) => console.error("Error fetching countries:", error));
-  }, []);
-
-  const handleTabChange = (tabName) => {
-    setActiveTab(tabName);
   };
 
   return (
     <div className="bg-white">
       <div className="grid w-full gap-x-6 gap-y-8 lg:gap-x-8 p-7 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        {success ? 
-        <div className="col-span-12">
-          <div className="flex justify-center">
-            <span className="flex mb-5 p-1 border rounded-full border-gray-300">
-              <div
-                className={`py-1 px-5 ${
-                  activeTab === "GUEST"
-                    ? "bg-green-800 rounded-full text-white font-medium tabTransition"
-                    : "font-medium"
-                }`}
-                onClick={() => handleTabChange("GUEST")}
-              >
-                Участник
-              </div>
-              <div
-                className={`py-1 px-5 ${
-                  activeTab === "PLAYER"
-                    ? "bg-green-800 rounded-full text-white font-medium tabTransition"
-                    : "font-medium"
-                }`}
-                onClick={() => handleTabChange("PLAYER")}
-              >
-                Предприниматель
-              </div>
-            </span>
-          </div>
-          <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">
-            Участвовать на выставке
-          </h2>
-          <Link
-            to={ABOUT_ROUTE}
-            className="text-sm text-gray-500 underline"
-            onClick={onHide}
-          >
-            Информация о выставке
-          </Link>
-                
-          {activeTab === "PLAYER" ? (
-            <section aria-labelledby="options-heading" className="mt-5">
-              <div className="grid w-full grid-cols-1 lg:grid-cols-12 space-x-2">
-                {/* Input fields for manual input */}
-                <DropdownSelect
-                  label="или выбрать страну"
-                  onChange={setCountry}
-                  selected={country}
-                  arrayList={countryes}
-                />
-                <InputField
-                  label="Название компании"
-                  value={companyName}
-                  onChange={setCompanyName}
-                  required
-                />
-                <InputField
-                  label="Адрес"
-                  value={address}
-                  onChange={setAddress}
-                />
-                <InputField label="E-mail" value={email} onChange={setEmail} />
-                <InputField label="Телефон" value={phone} onChange={setPhone} />
-                <InputField
-                  label="Имя сотрудника"
-                  value={employeeName}
-                  onChange={setEmployeeName}
-                />
-              </div>
-
-              <div className="grid w-full grid-cols-2 lg:grid-cols-12 gap-2">
-                {Array(1, 2, 3, 4).map((number, index) => (
-                  <div
-                    key={index}
-                    className="relative my-4 sm:col-span-6 lg:col-span-3"
-                  >
-                    <div
-                      className="border border-gray-300 rounded-md"
-                      style={{
-                        backgroundImage:
-                          "url('https://sc04.alicdn.com/kf/UTB8HHIEA9nEXKJk43Ubq6zLppXaC.jpg')",
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        height: "200px", // Set your desired height
-                      }}
-                    >
-                      <div className="absolute bottom-1 w-full p-2">
-                        <div className="rounded-full bg-white/70 grid grid-cols-12 p-2">
-                          <button className="bg-white text-gray-800 w-full py-1 rounded-full shadow-md col-span-4">
-                            -
-                          </button>
-                          <span className="text-black font-bold text-xl mx-2 col-span-4 text-center">
-                            0
-                          </span>
-                          <button className="bg-white text-gray-800 w-full py-1 rounded-full shadow-md col-span-4">
-                            +
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-
-              <div
-                className="
-                bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-600 cursor-pointer
-                mt-6 flex w-full items-center justify-center rounded-md border border-transparent px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Отправить
-              </div>
-            </section>
-          ) : (
-            <section aria-labelledby="options-heading" className="mt-5">
-              <div className="grid w-full grid-cols-1 lg:grid-cols-12 space-x-2">
-                
-                <DropdownSelect
-                  label="Ввыбрать страну"
-                  onChange={setCountry}
-                  selected={country}
-                  arrayList={countryes}
-                />
-                <InputField
-                  label="или введите страну"
-                  value={region}
-                  onChange={setRegion}
-                />
-                <InputField
-                  label="Полное имя"
-                  value={employeeName}
-                  onChange={setEmployeeName}
-                />
-                <InputField
-                  label="Телефонный номер"
-                  value={phoneNumber}
-                  onChange={setPhoneNumber}
-                />
-              </div>
-              <div
-                onClick={addParticipant}
-                className="
+        {!success ? (
+          <div className="col-span-12">
+            <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">
+              {translations[country.id].participateTitle}
+            </h2>
+            <Link
+              to={ABOUT_ROUTE}
+              className="text-sm text-gray-500 underline"
+              onClick={onHide}
+            >
+              {translations[country.id].aboutExhibition}
+            </Link>
+              <section aria-labelledby="options-heading" className="mt-5">
+                <div className="grid w-full grid-cols-1 lg:grid-cols-12 space-x-2">
+                  <DropdownSelect
+                    label={translations[country.id].countryLabel}
+                    onChange={setCountry}
+                    selected={country}
+                    arrayList={translations[country.id].countryOptions}
+                  />
+                  <InputField
+                    label={translations[country.id].fullNameLabel}
+                    value={employeeName}
+                    onChange={setEmployeeName}
+                  />
+                  <InputField
+                    label={translations[country.id].phoneNumberLabel}
+                    value={phoneNumber}
+                    onChange={setPhoneNumber}
+                  />
+                  <InputField
+                    label={translations[country.id].emailLabel}
+                    value={email}
+                    onChange={setEmail}
+                  />
+                </div>
+                <button
+                  onClick={addParticipant}
+                  className="
           bg-gradient-to-r from-green-800 to-green-900 hover:from-green-900 hover:to-green-900 cursor-pointer
           mt-6 flex w-full items-center justify-center rounded-md border border-transparent px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Отправить
-              </div>
-            </section>
-          ) }
-        </div>
-      : 
-      (
-        <div>
-            <div className=" rounded-lg bg-green-100 p-3">Вы успешно зарегистрировались </div>
-      <Link
-        to={SHOP_ROUTE}
-        className="text-sm text-gray-500 underline"
-        onClick={onHide}
-      >
-        Вернутся на главную страницы
-      </Link>
-        </div>
-      )}
+                >
+                  {translations[country.id].submitButton}
+                </button>
+              </section>
+          </div>
+        ) : (
+            <div className="flex flex-col items-center justify-center">
+                {translations[country.id].pleaseDontClose}
+            <div className="rounded-lg bg-green-100 p-6 text-center">
+              <p className="text-lg font-semibold">{translations[country.id].successMessage}</p>
+            </div>
+      
+            <div className="mt-6">
+              <QRCode
+                value={`https://uzexpo.com/participants/` + id}
+                size={200}
+                fgColor="#000000"
+                bgColor="#ffffff"
+                level="H"
+              />
+            </div>
+      
+            <Link
+              to={SHOP_ROUTE} // Предполагая, что SHOP_ROUTE определен
+              className="mt-6 text-sm text-gray-500 underline"
+              onClick={onHide}
+            >
+              {translations[country.id].returnHomeLink}
+            </Link>
+          </div>
+          
+        )}
       </div>
     </div>
   );
