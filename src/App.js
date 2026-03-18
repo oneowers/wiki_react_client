7f634cd43bff03e535a4c3d7a196052e06628078
oneowers <kamolov1575@gmail.com>
2026-03-18 10:49:02 +0500

    fix updates loading

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter.js';
 import NavBar from './components/NavBar.js';
 import { observer } from 'mobx-react-lite';
-import { useContext, useEffect, useState } from 'react';
-import { Context } from './index.js';
-import { check, user as userApi } from './http/userApi.js'; // Renamed to avoid naming conflict
+import { useEffect, useState } from 'react';
+import { check } from './http/userApi.js';
 import Footer from './components/Footer.js';
 
 const App = observer(() => {
-  const { user } = useContext(Context);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const checkData = await check();
+        await check();
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -30,7 +28,6 @@ const App = observer(() => {
     return (
       <div className="rabbit-loader">
         <span className="rabbit-label">Loading</span>
-
         <div className="rabbit-dots">
           <span /><span /><span />
         </div>
